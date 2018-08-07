e528133e8a6c9b151217f7553b62138c26c480f7
Luflosi <luflosi@luflosi.de>
2018-08-08 00:39:46 +0200

    Fix invalid syntax

--- i3wpd.py
+++ i3wpd.py
@@ -21,7 +21,7 @@ class i3_Wpd:
 
 		i3.subscribe(['workspace', 'shutdown', 'output'], self.focus_changed_handler)
 
-	def change_wallpaper(self)
+	def change_wallpaper(self):
 		dbg("Number of current wallpapers: " + len(self.current_files))
 		self.current_files = {file: random.choice(self.files) for file in self.current_files}
 
